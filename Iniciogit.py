dcd2d9c2771856f699cf5e529aafac2c0965690c
Pablo Angel Montaño Montaño <pmontano@unal.edu.co>
2022-05-16 12:35:57 -0500

    Se agrego un print

--- Iniciogit.py
+++ Iniciogit.py
@@ -2,3 +2,7 @@ for i in range(11):
     for j in range(1,3):
         print(f'Numero {i**(2*j)}')
         print(f'Numero{j**i}')
+
+for p in range(5):
+    print(f'Nada como ganar {p} partidos seguidos.')        
+    
